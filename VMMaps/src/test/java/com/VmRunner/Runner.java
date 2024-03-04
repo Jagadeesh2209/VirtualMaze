2b6eea7718f9a09683d5c3852d1f78c8f02476d8
Jagadeesh2209 <162190508+Jagadeesh2209@users.noreply.github.com>
2024-03-05 01:15:32 +0530

    Add files via upload
    Initial commit.

---
+++ VMMaps/src/test/java/com/VmRunner/Runner.java
@@ -0,0 +1,48 @@
+package com.VmRunner;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.VmTest.VMTest;
+import com.base.BaseClass;
+
+public class Runner extends BaseClass {
+
+	public static WebDriver driver;
+
+	@BeforeTest
+	public static void setUp() {
+		
+		driver=launchBrowser("chrome");
+		implicitWait(30);
+	}
+
+	@Test
+	public void executeTest() {
+
+		VMTest test = new VMTest();
+		test.VMWrongRegister();
+		test.VMRegisteration();
+		test.VMReRegisteration();
+		test.login();
+		test.logout();
+		test.wrongLogin1();
+		test.wrongLogin2();
+		test.forgetPass();
+		test.forgetpassotp();
+	
+	}
+	
+	
+	
+	@AfterTest
+	public static void teardown() throws Throwable {
+		quitBrowser();
+	}
+	
+}
